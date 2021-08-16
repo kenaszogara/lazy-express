794c13631de608fc02e21fc642140e728c13dbb7
kenaszogara <kenaszogara@live.com>
2021-08-16 23:09:46 +0800

    initial commit: added createController, createRoute

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
-const replace = require("buffer-replace");
 
 const params = process.argv;
 const BASE_PATH = params[1];
 const PARAMS_1 = params[2];
-const PARAMS_2 = params[3];
-
 console.log(params);
 
 function createModel(a) {
@@ -33,7 +30,7 @@ function createModel(a) {
     // table_name
     output = output.replace("$$TABLE_NAME$$", tableName);
 
-    // output to src file
+    // write model file
     fs.writeFile(destination, output, function (err) {
       if (err) return console.log(err);
       console.log(`src > models > ${filename}`);
@@ -45,4 +42,84 @@ function createModel(a) {
   }
 }
 
+function createController(a) {
+  if (a == undefined) return console.log("error");
+
+  try {
+    let output = "";
+    const modelName = a.charAt(0).toUpperCase() + a.slice(1);
+    const tableName = a.toLowerCase();
+    const filename = `${tableName}_controller.js`;
+    const destination = `${__dirname}/src/controllers/${filename}`;
+
+    const data = fs.readFileSync("./lib/template/controller", "utf8");
+    output = data;
+
+    // import model
+
+    // model_name
+    // output = output.replace("$$MODEL_NAME$$", modelName);
+    for (let i = 0; i < 8; i++) {
+      output = output.replace("$$MODEL_NAME$$", modelName);
+    }
+
+    // table_name
+    // output = output.replace("$$TABLE_NAME$$", tableName);
+    for (let i = 0; i < 6; i++) {
+      output = output.replace("$$TABLE_NAME$$", tableName);
+    }
+
+    // write model file
+    fs.writeFile(destination, output, function (err) {
+      if (err) return console.log(err);
+      console.log(`src > controllers > ${filename}`);
+    });
+
+    console.log(output);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function createRoute(a) {
+  if (a == undefined) return console.log("error");
+
+  try {
+    let output = "";
+    const modelName = a.charAt(0).toUpperCase() + a.slice(1);
+    const tableName = a.toLowerCase();
+    const filename = `${tableName}_route.js`;
+    const destination = `${__dirname}/src/routes/${filename}`;
+
+    const data = fs.readFileSync("./lib/template/route", "utf8");
+    output = data;
+
+    // import model
+
+    // model_name
+    // output = output.replace("$$MODEL_NAME$$", modelName);
+    for (let i = 0; i < 1; i++) {
+      output = output.replace("$$MODEL_NAME$$", modelName);
+    }
+
+    // table_name
+    // output = output.replace("$$TABLE_NAME$$", tableName);
+    for (let i = 0; i < 1; i++) {
+      output = output.replace("$$TABLE_NAME$$", tableName);
+    }
+
+    // write model file
+    fs.writeFile(destination, output, function (err) {
+      if (err) return console.log(err);
+      console.log(`src > routes > ${filename}`);
+    });
+
+    console.log(output);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 createModel(PARAMS_1);
+createController(PARAMS_1);
+createRoute(PARAMS_1);
